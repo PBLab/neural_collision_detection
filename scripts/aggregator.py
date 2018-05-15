5162399af9b00b876a8dbc7e8862be33aead945d
Yoav <yoav@example.com>
2018-05-15 21:49:40 +0300

    Use bisect search for better performance

--- scripts/aggregator.py
+++ scripts/aggregator.py
@@ -2,6 +2,7 @@ import sys, os
 import math
 import numpy as np
 import time
+import bisect
 
 
 def read_balls(fname):
@@ -116,30 +117,47 @@ def distance(n, v):
 	d = d ** 0.5
 	return d
 
+def sort_vascular(vascular):
+	vascular.sort(key = lambda v:v[0])
+
+def collide(n, v):
+		rsum = n[3] + v[3]
+		if abs(n[0] - v[0]) > rsum:
+			return False
+		if abs(n[1] - v[1]) > rsum:
+			return False
+		if abs(n[2] - v[2]) > rsum:
+			return False
+		if distance(n, v) <= rsum:
+			return True
+
 def find_nearest_points(vascular, neuron):
-	i = 0
 	collisions = []
 	print "len(neuron): ", len(neuron)
 
+	sort_vascular(vascular)
+
 	for n in neuron:
-		i += 1
-		if i % 500 == 0:
-			#print i
-			pass
-		for v in vascular:
-			rsum = n[3] + v[3]
-			if abs(n[0] - v[0]) > rsum:
-				continue
-			if abs(n[1] - v[1]) > rsum:
-				continue
-			if abs(n[2] - v[2]) > rsum:
-				continue
-			if distance(n, v) <= rsum:
-				#print n, v
-				#cnt += 1
-				#collisions.append(n)
-				collisions.append(v)
+		base_idx = bisect.bisect(vascular, n)
+		max_x_distance = 25 + n[3]
+		cur_idx = base_idx
+		found = False
+		while cur_idx < len(vascular) and abs(vascular[cur_idx][0]-n[0]) < max_x_distance:
+			v = vascular[cur_idx]
+			if collide(n, v):
+				collisions.append(n)
+				found = True
 				break
+			cur_idx += 1
+		if found:
+			continue
+		cur_idx = base_idx - 1
+		while cur_idx >= 0 and abs(vascular[cur_idx][0] - n[0]) < max_x_distance:
+			v = vascular[cur_idx]
+			if collide(n, v):
+				collisions.append(n)
+				break
+			cur_idx -= 1
 
 	#print "cnt = ", cnt
 	return collisions
@@ -158,6 +176,8 @@ def main(argv):
 	
 	print "Read vascular data..."
 	vascular = read_balls(vascular_filename)
+	max_r = max(vascular, key=lambda x : x[3])
+	print max_r
 	print "Read neuron data..."
 	neuron = read_balls(neuron_filename)
 
@@ -182,7 +202,7 @@ def main(argv):
 	with open(results_filename, "wb") as f:
 		#f.write(str(len(collisions)) + "\n")
 		for col in collisions:
-			f.write("%i,%i,%i\n" % (col[0], col[1], col[2]))
+			f.write("%f,%f,%f\n" % (col[0], col[1], col[2]))
 
 	print "Done!"
 	return 0
