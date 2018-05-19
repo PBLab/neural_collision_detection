7e7ebfeb94c50887885097c1fb12881d69903292
Yoav Jacobson <yoavj@stromboli.tau.ac.il>
2018-05-19 12:01:22 +0300

    Improve parser to use result class instead of simple lines

--- scripts/parser.py
+++ scripts/parser.py
@@ -2,13 +2,54 @@
 
 import os, sys
 
+class Result:
+	def __init__(self, line):
+		if isinstance(line, str):
+			self.init_from_string(line)
+		elif isinstance(line, Result):
+			self.init_from_result(line)
+
+	def init_from_result(self, res):
+		self.run_id = res.run_id
+		self.neuron_id = res.neuron_id
+		self.vascular_id = res.vascular_id
+		self.translation = res.translation
+		self.rotation = res.rotation
+		self.collisions = list(res.collisions)
+
+	def init_from_string(self, line):
+		splitted = line.split(",")
+		if len(splitted) != 6:
+			raise Exception("bad line")
+
+		self.run_id = splitted[0]
+		self.neuron_id = splitted[1]
+		self.vascular_id = splitted[2]
+		self.translation = [float(x) for x in splitted[3].split(" ")]
+		self.rotation = [float(x) for x in splitted[4].split(" ")]
+		self.collisions = []
+		for col in splitted[5].split("|"):
+			self.collisions.append([float(x) for x in col.split(" ")])
+
+	def get_collision_string(self):
+		res = ""
+		for col in self.collisions:
+			x, y, z = [str(a) for a in col]
+			col = " ".join([x, y, z])
+			res += col + "|"
+		return res[:-1]
+
+	def __str__(self):
+		col_str = self.get_collision_string()
+		translation_str = " ".join([str(a) for a in self.translation])
+		rotation_str = " ".join([str(a) for a in self.rotation])
+		return "{0},{1},{2},{3},{4},{5}".format(self.run_id, self.neuron_id, self.vascular_id, translation_str, rotation_str, col_str)
+
 class ResultsParser:
 	def __init__(self, lines):
-		#with open(fname, "rb") as f:
-			#self.lines = f.readlines()
-		self.lines = lines
+		self.results = [Result(l) for l in lines]
 		self.idx = 0
-		self.total_len = len(lines)
+		self.total_len = len(self.results)
 
 	def __len__(self):
 		return self.total_len
@@ -21,15 +62,17 @@ class ResultsParser:
 		if self.idx >= self.total_len:
 			raise StopIteration
 		self.idx += 1
-		return self.lines[self.idx - 1]
+		return self.results[self.idx - 1]
 
 	def where(self, f):
-		lines = []
-		for l in self.lines:
-			if f(l):
-				lines.append(l)
-		return ResultsParser(lines)
+		results = []
+		for r in self.results:
+			if f(r):
+				results.append(r)
+		return ResultsParser(results)
 		
+	def __str__(self):
+		return "\n".join([str(x) for x in self.results])
 
 
 def main(argv):
@@ -42,9 +85,8 @@ def main(argv):
 	parser = ResultsParser(open(input_filename).readlines())
 	print len(parser)
 
-	COL_COLLISIONS = 5
-	print "\n".join(parser.where(lambda x: x.startswith("agg_distance_0,")).\
-						where(lambda x : len(x.split(",")[COL_COLLISIONS].split("|")) < 6))
+	print parser.where(lambda x: x.run_id == "agg_distance_0").where(lambda x : len(x.collisions) < 6)
+	#print "\n".join(parser.where(lambda x: x.run_id == "agg_distance_0").where(lambda x : len(x.collisions) < 6))
 	#print "\n".join(parser.where(lambda x: x.startswith("agg_distance_0,")).where(lambda x : len(x.split(",")[COL_COLLISIONS].split("|")) <= 10).lines)
 	
 if __name__ == "__main__":
