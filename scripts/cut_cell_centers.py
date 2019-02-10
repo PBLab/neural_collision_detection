3e66106defc3c308d2dd2f63a353e13e6268edf0
HagaiHargil <hagaihargil@gmail.com>
2019-02-10 15:38:04 +0200

    FEATURE: Mapping between neuron and cortical layer - resolves #8

--- scripts/cut_cell_centers.py
+++ scripts/cut_cell_centers.py
@@ -8,6 +8,8 @@ import numpy as np
 
 
 class CorticalLayer(Enum):
+    """ Depth of cortical layers in um """
+
     ONE = (0, 80)
     TWOTHREE = (80, 350)
     FOUR = (350, 500)
@@ -16,22 +18,36 @@ class CorticalLayer(Enum):
 
 
 def read_csv(fname, downsample=1):
-    data = pd.read_csv(fname, header=None, names=['x', 'y', 'z'])
+    data = pd.read_csv(fname, header=None, names=["x", "y", "z"])
     data = data.iloc[::downsample, :]
     return data
 
 
 def get_neurons_from_layer(data: pd.DataFrame, layer: CorticalLayer):
-    relevant_rows = (data['z'] >= layer.value[0]) & (data['z'] < layer.value[1])
+    relevant_rows = (data["z"] >= layer.value[0]) & (data["z"] < layer.value[1])
     data = data.loc[relevant_rows, :]
     return data
 
 
+def save_cell_centers_array(
+    data: pd.DataFrame, layer: CorticalLayer, num_to_save: int = 10000
+):
+    num_to_save = min(num_to_save, data.shape[0])
+    indices = np.random.choice(data.shape[0], num_to_save, replace=False)
+    data = data.iloc[indices, :]
+    data.to_csv(f"centers_layer_{layer.name}.csv", header=None, index=False)
+
+
 if __name__ == "__main__":
-    fname = '/data/simulated_morph_data/vascular/all_cell_centers.csv'
+    fname = "/data/simulated_morph_data/vascular/all_cell_centers.csv"
     data = read_csv(fname, downsample=1)
-    for layer in [CorticalLayer.ONE, CorticalLayer.TWOTHREE,
-                  CorticalLayer.FOUR, CorticalLayer.FIVE,
-                  CorticalLayer.SIX]:
+    for layer in [
+        CorticalLayer.ONE,
+        CorticalLayer.TWOTHREE,
+        CorticalLayer.FOUR,
+        CorticalLayer.FIVE,
+        CorticalLayer.SIX,
+    ]:
         cur_data = get_neurons_from_layer(data, layer)
-        cur_data.to_csv(f'centers_layer_{layer.name}.csv')
+        save_cell_centers_array(cur_data, layer)
+
