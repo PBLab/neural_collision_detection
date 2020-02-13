2186071bee56d1a140e5388cf8f3422e3edd8ccf
Yoav <yoav@example.com>
2020-02-14 00:50:03 +0200

    Add external dependencies

--- src/ncd_core/fcl.hpp
+++ src/ncd_core/fcl.hpp
@@ -1,11 +1,14 @@
 #pragma once
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 #include "fcl/geometry/bvh/BVH_model.h"
 #include "fcl/math/bv/OBBRSS.h"
 #include "fcl/math/bv/AABB.h"
 #include "fcl/narrowphase/collision.h"
 #include "fcl/common/types.h"
 #include <vector>
+#pragma GCC diagnostic pop
 
 using namespace fcl;
 using namespace std;
