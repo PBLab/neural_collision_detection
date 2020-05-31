be7fd6bc378164fecd091df3680ae8d9dd9f8ccc
Hagai Har-Gil <=>
2020-05-31 13:35:35 +0300

    Added serialization of graphs of alpha vs collisions

--- src/ncd_post_process/alpha_shapes/find_block_properties.py
+++ src/ncd_post_process/alpha_shapes/find_block_properties.py
@@ -10,6 +10,7 @@ import numpy as np
 import pandas as pd
 import scipy.stats
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 from ncd_post_process.create_neuron_id.collisions_vs_dist_naive import (
     CollisionsDistNaive,
@@ -31,7 +32,6 @@ def find_points_bounding_box(points: pd.DataFrame) -> tuple:
     """
     x_max, y_max, z_max = points.max(axis=0)
     x_min, y_min, z_min = points.min(axis=0)
-    print(f"The size of")
     return ((x_min, x_max), (y_min, y_max), (z_min, z_max))
 
 
@@ -80,9 +80,40 @@ def divide_neuron_into_blocks(points: pd.DataFrame, blocks=(10, 10, 5)) -> np.nd
     return ret[2]
 
 
-def plot_distribution(data: pd.DataFrame):
-    """Plots a jointplot of the data, dividing
-    axons and dendrites.
+def divide_and_plot(all_data: pd.DataFrame):
+    """Divides the given data frame into axonal and dendritic blocks,
+    and plots them together on the same JointPlot.
+
+    The functions tries to deal with situations where there are only axonal points
+    and\or dendritic points.
+
+    Parameters
+    ----------
+    all_data : pd.DataFrame
+        A DF consisting of both axonal and dendritic data with a "type"
+        column that separates them.
+    """
+    all_data.loc[:, "is_ax"] = all_data["type"].str.contains("Axon")
+    axonal = all_data.query("is_ax == True")
+    dendritic = all_data.query("is_ax == False")
+    both = 0
+    if not axonal.empty:
+        g, bins = plot_distribution(axonal, color="C2")
+        both += 1
+    else:
+        g = sns.JointGrid(x="coll", y="alpha", data=dendritic)
+        bins = None
+    if not dendritic.empty:
+        g, _ = plot_distribution(dendritic, color="C1", g=g, bins=bins)
+        both += 1
+    if both == 2:
+        bb = find_points_bounding_box(all_data.loc[:, "x":"z"])
+        g.fig.suptitle("Alpha Value vs. Collision Chance for Axons (Green) and Dendrites")
+        g.fig.savefig(f'/data/neural_collision_detection/results/for_article/fig2/coll_vs_alpha_{bb}.png', dpi=300, transparent=True)
+
+
+def plot_distribution(data: pd.DataFrame, color, g=None, bins=None):
+    """Plots a jointplot of the data.
 
     This plot is of a single block, showing the relation between
     the collision chance and the alpha value.
@@ -91,8 +122,23 @@ def plot_distribution(data: pd.DataFrame):
     ----------
     data : pd.DataFrame
         Table with collision and alpha information
+    color : str
+        Color of the elements
+    g : sns.JointGrid, optional
+        The JointGrid on which we'll draw. Creates it if it doesn't exist
+    bins : dict, optional
+        A dictionary with 'x' and 'y' keys containing the bin edges
     """
-    sns.jointplot(data=data, x="collisions", y="alpha")
+    if not g:
+        g = sns.JointGrid(x="coll", y="alpha", data=data)
+    if not bins:
+        bins = dict(x=None, y=None)
+    g.ax_joint.scatter(data["coll"], data["alpha"], c=color, s=7, alpha=0.6)
+    _, binx, _ = g.ax_marg_x.hist(data["coll"], alpha=0.6, color=color, bins=bins["x"])
+    _, biny, _ = g.ax_marg_y.hist(
+        data["alpha"], alpha=0.6, color=color, orientation="horizontal", bins=bins["y"]
+    )
+    return g, {"x": binx, "y": biny}
 
 
 if __name__ == "__main__":
@@ -100,8 +146,11 @@ if __name__ == "__main__":
         "results/2020_02_14/graph_AP120410_s1c1_with_collisions.gml"
     ).resolve()
     points = load_neuronal_points(path, "AP120410")
-    indices = divide_neuron_into_blocks(points.loc[:, "x":"z"], (10, 10, 5))
+    indices = divide_neuron_into_blocks(points.loc[:, "x":"z"], (10, 19, 6))
     uniques = np.unique(indices)
-    blocks = (points.iloc[unique] for unique in uniques)
+    blocks = (points.iloc[indices == unique] for unique in uniques)
     with multiprocessing.Pool() as mp:
-        mp.map(plot_distribution, blocks)
+        mp.map(divide_and_plot, blocks)
+    # for block in blocks:
+    #     divide_and_plot(block)
+    #     plt.show()
