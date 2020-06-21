cf1539b5a4466c815d7aa36c570c8b1b010c0967
Hagai Har-Gil <=>
2020-06-21 11:51:00 +0300

    Add OLS results object to serialization

--- src/ncd_post_process/alpha_shapes/find_block_properties.py
+++ src/ncd_post_process/alpha_shapes/find_block_properties.py
@@ -6,7 +6,7 @@ axon-dendrite relations in the block and more."""
 import pathlib
 import multiprocessing
 from typing import Tuple, List
-from collections import defaultdict
+from collections import namedtuple, defaultdict
 import pickle
 
 import numpy as np
@@ -19,6 +19,9 @@ import statsmodels.api as sm
 from ncd_post_process.create_neuron_id.collisions_vs_dist_naive import (
     CollisionsDistNaive,
 )
+
+
+LMResults = namedtuple('LMResults', 'r2, const, slope')
 
 
 def find_points_bounding_box(points: pd.DataFrame) -> tuple:
@@ -108,21 +111,27 @@ def divide_and_plot(
     dendritic = all_data.query("is_ax == False")
     both = 0
     if not axonal.empty:
-        g, bins = plot_distribution(axonal, color="C2")
+        g, bins, ax_lmresult = plot_distribution(axonal, color="C2")
         both += 1
     else:
         g = sns.JointGrid(x="alpha", y="coll", data=dendritic)
         bins = None
     if not dendritic.empty:
-        g, _ = plot_distribution(dendritic, color="C1", g=g, bins=bins)
+        g, _, dend_lmresult = plot_distribution(dendritic, color="C1", g=g, bins=bins)
         both += 1
     if both == 2:
         bb = find_points_bounding_box(all_data.loc[:, "x":"z"])
-        _serialize_data(data_folder, bb, neuron_name)
+        _serialize_data(data_folder, bb, neuron_name, (ax_lmresult, dend_lmresult))
         _serialize_fig(data_folder, bb, neuron_name, g.fig)
 
 
-def _serialize_data(data_folder: pathlib.Path, bb: tuple, neuron_name: str):
+def _default_results_dict() -> dict:
+    """Callable that returns a specialized 'data structure'
+    for safe-keeping the results of this computation"""
+    return {'bounding_boxes': [], 'lmresults': None}
+
+
+def _serialize_data(data_folder: pathlib.Path, bb: tuple, neuron_name: str, lmresults: Tuple[sm.OLSResults]):
     """Writes the given bounding box data to disk.
     
     The data is one of the bounding boxes of the current neuron, and it should
@@ -137,15 +146,19 @@ def _serialize_data(data_folder: pathlib.Path, bb: tuple, neuron_name: str):
     bb : 3-tuple of 2-tuple
         For each axis, start and end of bounding box
     neuron_name : str
+    lmresults : 2-tuple of sm.OLSResults
+        Axonal and dendritic results of the linear regression
     """
     pickle_fname = data_folder / "bb_coord_alpha_coll.pickle"
     try:
         with open(pickle_fname, "rb") as f:
             data = pickle.load(f)
     except FileNotFoundError:
-        data = defaultdict(list, {})
-    if data not in data[neuron_name]:
-        data[neuron_name].append(bb)
+        data = defaultdict(_default_results_dict, _default_results_dict())
+    if bb not in data[neuron_name]["bounding_boxes"]:
+        data[neuron_name]["bounding_boxes"].append(bb)
+        result = LMResults(lmresults.rsquared, lmresults.params[0], lmresults.params[1])
+        data[neuron_name]["lmresults"] = result
     with open(pickle_fname, "w+b") as f:
         pickle.dump(data, f)
 
@@ -172,7 +185,7 @@ def _serialize_fig(
     fig.savefig(data_folder / fig_fname, dpi=300, transparent=True)
 
 
-def plot_distribution(data: pd.DataFrame, color, g=None, bins=None):
+def plot_distribution(data: pd.DataFrame, color, g=None, bins=None) -> Tuple:
     """Plots a jointplot of the data.
 
     This plot is of a single block, showing the relation between
@@ -188,6 +201,15 @@ def plot_distribution(data: pd.DataFrame, color, g=None, bins=None):
         The JointGrid on which we'll draw. Creates it if it doesn't exist
     bins : dict, optional
         A dictionary with 'x' and 'y' keys containing the bin edges
+
+    Returns
+    -------
+    g : sns.JointGrid
+        The grid seaborn object we're plotting with
+    dict
+        A dictionary with the ["x"] and ["y"] bins of the histograms
+    lmresult : sm.OLSResults, optional
+        The results of the linear regression
     """
     if not g:
         g = sns.JointGrid(x="alpha", y="coll", data=data)
@@ -195,22 +217,22 @@ def plot_distribution(data: pd.DataFrame, color, g=None, bins=None):
         bins = dict(x=None, y=None)
     if len(data) < 5:
         g.ax_joint.scatter(data["alpha"], data["coll"], c=color, s=7, alpha=0.6)
+        lmresult = None
     else:
-        _calc_and_plot_regression(data, color, g.ax_joint)
+        lmresult = _calc_regression(data)
+        _plot_regression(data, color, g.ax_joint, lmresult)
     _, binx, _ = g.ax_marg_x.hist(data["alpha"], alpha=0.6, color=color, bins=bins["x"])
     _, biny, _ = g.ax_marg_y.hist(
         data["coll"], alpha=0.6, color=color, orientation="horizontal", bins=bins["y"]
     )
-    return g, {"x": binx, "y": biny}
+    return g, {"x": binx, "y": biny}, lmresult
 
 
-def _calc_and_plot_regression(data: pd.DataFrame, color: str, ax: plt.Axes):
+def _plot_regression(data: pd.DataFrame, color: str, ax: plt.Axes, result: sm.OLSResults):
     """Plots a regression line with the scattered point for the given data.
 
-    This method assumes that the given data has enough points to be used in a
-    regression analysis.
-    It displays the regression using Seaborn and calculates the actual R2 value
-    using statsmodels, and then shows it on the figure.
+    This method assumes that statsmodels was already used to calculate the regression
+    parameters, and now it merely has to display them (using seaborn).
 
     Parameters
     ----------
@@ -220,6 +242,8 @@ def _calc_and_plot_regression(data: pd.DataFrame, color: str, ax: plt.Axes):
         Color of the current plotted points
     ax : plt.Axes
         Axis to draw on
+    result : sm.OLSResults
+        Results object from statsmodels
     """
     locations = {"C1": (0.8, 0.9), "C2": (0.8, 0.8)}
     no_nulls = data.dropna()
@@ -231,9 +255,7 @@ def _calc_and_plot_regression(data: pd.DataFrame, color: str, ax: plt.Axes):
         scatter_kws={"s": 7, "alpha": 0.6},
         ax=ax,
     )
-    mod = sm.OLS(no_nulls["coll"], no_nulls["alpha"])  # y then x
-    res = mod.fit()
-    r = res.rsquared
+    r = result.rsquared
 
     plt.text(
         *locations[color],
@@ -244,6 +266,25 @@ def _calc_and_plot_regression(data: pd.DataFrame, color: str, ax: plt.Axes):
         transform=ax.transAxes,
         color=color,
     )
+    return res
+
+
+def _calc_regression(data: pd.DataFrame) -> sm.OLSResults:
+    """Calculate an ordinary linear regression for the given data.
+    
+    Parameters
+    ----------
+    data : pd.DataFrame
+        Alpha shapes and collision chance data
+
+    Returns
+    -------
+    sm.OLSResults
+    """
+    no_nulls = data.dropna()
+    mod = sm.OLS(sm.add_constant(no_nulls["coll"]), no_nulls["alpha"])  # y then x
+    res = mod.fit()
+    return res
 
 
 def main(neuron_name: str, data_folder: pathlib.Path, blocks: tuple):
@@ -283,4 +324,4 @@ if __name__ == "__main__":
         "/data/neural_collision_detection/results/for_article/fig2"
     )
     neuron_name = "AP120410_s1c1"
-    main(neuron_name, data_folder, (10, 19, 6))
+      sm.OLSResultsm.OLSResultsain(neuron_name, data_folder, (10, 19, 6))
