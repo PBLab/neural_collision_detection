ce22b9892b51a413cc7e22f295a720934060154f
Hagai Har-Gil <=>
2020-10-04 11:35:42 +0300

    Removed old pre-processing modules

--- src/convert_obj_matlab/circle3d.m
+++
@@ -1,7 +0,0 @@
-function mycircle = circle3d(circle_size);
-        myhalf = round(circle_size-1)/2;
-        [x,y,z] = meshgrid(-myhalf:myhalf,-myhalf:myhalf,-myhalf:myhalf);
-        mycircle = sqrt(x.^2 + y.^2 +z.^2);
-        mycircle = mycircle<=(circle_size/2);
- return
-        
