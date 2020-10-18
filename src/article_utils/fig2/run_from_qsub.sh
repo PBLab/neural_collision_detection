c39297fd73e5668d8cae1407026338641c2abb09
Hagai Har-Gil <=>
2020-10-18 17:55:20 +0300

    Removed MATLAB-based alpha shape calculation

--- src/article_utils/fig2/run_from_qsub.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#PBS -e /tmp/pblab/matlab/output
-#PBS -o /tmp/dvory/matlab/output
-
-matlab -nodisplay -r /data/neural_collision_detection/src/article_utils/fig2/run_alpha_shape_for_all_neurons.m
--- src/article_utils/fig2/save_results.m
+++
@@ -1,7 +0,0 @@
-function save_results(first_alpha_per_point, neuron_name)
-% Save the given results to a folder
-    foldername = '/data/neural_collision_detection/results/2020_02_14/';
-    filename = strcat(foldername, neuron_name, '_alpha_values.mat');
-    save(filename, 'first_alpha_per_point');
-end
-
